61e44c303036dc1f7a249a30fba9fdd196f47860
Jakub Mikolajczyk <Mikolajczyk.Jakub@outlook.com>
2021-07-03 00:49:40 +0200

    ClientPanel change

---
+++ src/main/java/Box.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Box extends JPanel {//make box 2x1
+
+
+    public Box(Object obj1, Object obj2){
+
+        setLayout(new GridLayout(0,1));
+        add((Component) obj1);
+        add((Component) obj2);
+
+    }
+
+}
